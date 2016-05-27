3ebbb99158559abec0e2d3fda38d415036abd844
mattray-mbr <mattray.mbr@gmail.com>
2016-05-27 11:34:22 -0600

    added base functionality

--- client/main.js
+++ client/main.js
@@ -1,8 +1,12 @@
 
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import todosList from '../imports/components/todosList/todosList';
 
-angular.module('simple-todos', [angularMeteor]);
+angular.module('simple-todos', [
+    angularMeteor, 
+    todosList.name
+]);
 
 
 
--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -2,15 +2,29 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import template from './todosList.html';
 
+import { Tasks } from '../../api/tasks.js'; //direct connection to the mongo collection without adding code to the server file
+
 class TodosListCtrl {
-    constructor() {
-        this.tasks = [{
-            text: 'This is task 1'
-        }, {
-            text: 'This is task 2'
-        }, {
-            text: 'This is task 3'
-        }];
+    constructor($scope) {
+        $scope.viewModel(this);
+
+        this.helpers({
+            tasks() {
+                return Tasks.find({}, {
+                    sort: {
+                        createdAt: -1
+                    } //sorts in reverse order
+                }); //finds and updates data from the mongo collection
+            }
+        })
+    }
+    //adds a new task to the db directly from the front end
+    addTask(newTask) {
+        Tasks.insert({
+            text: newTask,
+            createdAt: new Date //add whatever properties you want to object, there is no schema defined for the DB
+        });
+        this.newTask = ''; //clears the task form
     }
 }
 
@@ -18,6 +32,6 @@ export default angular.module('todosList', [
     angularMeteor
     ]) //used to seeing this as app
     .component('todosList', { //somewhat simmilar syntax to angular routing
-        templateUrl: 'imports/components/todosList/todosList.html',
-        controller: TodosListCtrl
+        templateUrl: 'imports/components/todosList/todosList.html', //meteor automatically parses this file and puts it in the body of the main html file
+        controller: ['$scope', TodosListCtrl]
     });
